2b6589e87eac916517045f8edb6cd9cb82c95fe0
sachin <sachinmcajnu@gmail.com>
2022-12-17 22:46:21 +0530

    add more colours to components, fill them with life

---
+++ src/app/recipe/model/recipe.model.ts
@@ -0,0 +1,14 @@
+export class Recipe{
+    public name:string;
+    public description:string;
+    public imagePath:string;
+
+  constructor(n:string, d:string, p:string){
+    this.name = n;
+    this.description= d;
+    this.imagePath = p;
+  }
+
+
+}
+
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients:Ingredient[] = [
+    new Ingredient("apple",5),
+    new Ingredient("Tomato",10)
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onShoppingItemClicked(name:string){
+      
   }
 
 }
